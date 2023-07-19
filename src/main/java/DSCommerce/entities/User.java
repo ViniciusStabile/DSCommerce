409a32ddd348be831b24c4b96a0b4fd585038f1f
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-07-19 17:31:29 -0300

    configured h2 database

--- src/main/java/DSCommerce/entities/User.java
+++ src/main/java/DSCommerce/entities/User.java
@@ -3,8 +3,18 @@ package DSCommerce.entities;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
