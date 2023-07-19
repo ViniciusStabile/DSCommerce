a0371ecd85de4c61e109969c824263fecb77f9ad
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-07-19 17:13:50 -0300

    Inicial project

---
+++ src/test/java/DSCommerce/DsCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package DSCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
