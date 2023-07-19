8fcf3cb7c1b4592afbccdc75f819e61a0adcd7d8
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-07-19 18:19:37 -0300

    Order, OrderStatus,Payment

---
+++ src/main/java/DSCommerce/entities/Order.java
@@ -0,0 +1,104 @@
+package DSCommerce.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
+	private OrderStatus status;
+
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+
+	@OneToOne(mappedBy = "order")
+	private Payment payment;
+
+	public Order() {
+
+	}
+
+	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+		this.payment = payment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/main/java/DSCommerce/entities/OrderStatus.java
@@ -0,0 +1,7 @@
+package DSCommerce.entities;
+
+public enum OrderStatus {
+
+	WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+
+}
