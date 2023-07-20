225f59fdb8e5b979595fce5a8c0f66e0ad3887a7
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-07-20 20:04:41 -0300

    Insert

--- src/main/java/DSCommerce/controllers/ProductController.java
+++ src/main/java/DSCommerce/controllers/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,13 @@ public class ProductController {
 	public ProductDTO findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
 		return dto;
+	}
+	
+	@PostMapping
+	public ProductDTO insert(@RequestBody ProductDTO dto) {
+		return service.insert(dto);
+		
+		
 	}
 
 }
--- src/main/java/DSCommerce/services/ProductService.java
+++ src/main/java/DSCommerce/services/ProductService.java
@@ -25,9 +25,20 @@ public class ProductService {
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAll(Pageable pageable) {
 		Page<Product> result = repository.findAll(pageable);
-		return result.map(x  -> new ProductDTO(x));
+		return result.map(x -> new ProductDTO(x));
 	}
 
-	
-	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+
+	}
+
 }
