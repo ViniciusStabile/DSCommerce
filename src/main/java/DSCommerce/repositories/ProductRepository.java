81b66fd0f545c59164dd55249f5d4f9cc2447e1a
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-08-03 16:21:17 -0300

    searchByName

--- src/main/java/DSCommerce/repositories/ProductRepository.java
+++ src/main/java/DSCommerce/repositories/ProductRepository.java
@@ -1,9 +1,17 @@
 package DSCommerce.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import DSCommerce.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	@Query("SELECT obj FROM Product obj "
+			+ "WHERE UPPER(obj.name) "
+			+ "LIKE UPPER (CONCAT('%', :name, '%'))")
+	Page<Product> serchByName(String name, Pageable pageable);
+	
 }
